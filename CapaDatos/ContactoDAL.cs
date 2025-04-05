c1396c792828c057e663f0b6e0b6ca70dde8075f
pablodls04 <pablomiguel200410@gmail.com>
2025-04-04 20:46:12 -0400

    Agregando carpeta

---
+++ CapaDatos/ContactoDAL.cs
@@ -0,0 +1,86 @@
+﻿using CapaEntidad;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+
+namespace CapaDatos
+{
+    public class ContactoDAL
+    {
+        private string connectionString = ConfigurationManager.ConnectionStrings["ConexionBD"].ConnectionString;
+
+        public void Insertar(Contacto contacto)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("InsertarContacto", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Nombre", contacto.Nombre);
+                cmd.Parameters.AddWithValue("@Telefono", contacto.Telefono);
+                cmd.Parameters.AddWithValue("@Email", contacto.Email);
+                cmd.Parameters.AddWithValue("@Direccion", contacto.Direccion);
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public void Modificar(Contacto contacto)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("ModificarContacto", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Id", contacto.Id);
+                cmd.Parameters.AddWithValue("@Nombre", contacto.Nombre);
+                cmd.Parameters.AddWithValue("@Telefono", contacto.Telefono);
+                cmd.Parameters.AddWithValue("@Email", contacto.Email);
+                cmd.Parameters.AddWithValue("@Direccion", contacto.Direccion);
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public void Eliminar(int id)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("EliminarContacto", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Id", id);
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public List<Contacto> Buscar(string nombre)
+        {
+            List<Contacto> contactos = new List<Contacto>();
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("BuscarContacto", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Nombre", nombre);
+                con.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    contactos.Add(new Contacto
+                    {
+                        Id = Convert.ToInt32(reader["Id"]),
+                        Nombre = reader["Nombre"].ToString(),
+                        Telefono = reader["Telefono"].ToString(),
+                        Email = reader["Email"].ToString(),
+                        Direccion = reader["Direccion"].ToString()
+                    });
+                }
+            }
+            return contactos;
+        }
+    }
+}
